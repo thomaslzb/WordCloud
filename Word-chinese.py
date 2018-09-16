63838953b01aeda4523f065150a206eedbb5aa3a
thomaslzb <thomaslzb@users.noreply.github.com>
2018-09-16 15:41:38 +0100

    ver 0.1

---
+++ Word-chinese.py
@@ -0,0 +1,86 @@
+#-*-coding:utf-8-*-
+
+import jieba
+import jieba.analyse
+# jieba.enable_parallel(4)
+# 开启并行分词模式，参数为并行进程数:4 ,but unable to run on Windows
+
+from os import path
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+import os
+from wordcloud import WordCloud, ImageColorGenerator
+
+
+# The function for processing text with Jieba
+# 前50个高频词汇
+def jieba_processing_txt(text):
+    result = jieba.analyse.textrank(text, topK=50, withWeight=True)
+    keywords = dict()
+    for i in result:
+        keywords[i[0]] = i[1]
+    return keywords
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+stopwords_path = d + '/files/stopwords.txt'
+
+# Chinese fonts must be set 这个字库必须要
+font_path = d + '/files/fonts/SourceHanSerifK-Light.otf'
+
+# the path to save worldcloud
+imgname1 = d + '/files/LuXun.jpg'
+imgname2 = d + '/files/LuXun_colored.jpg'
+
+#files is exist?
+if not (os.path.isfile(stopwords_path) and os.path.isfile(font_path) and \
+        os.path.isfile(imgname1) ) :
+    if not os.path.isfile(stopwords_path):
+        print ("stopwords_path or pngFile does not found!")
+    if not os.path.isfile(font_path):
+        print ("font_path or pngFile does not found!")
+    if not os.path.isfile(imgname1):
+        print ("imgname1 or pngFile does not found!")
+    os._exit(0)
+
+# read the mask / color image taken from
+back_coloring = imread(path.join(d, d + '/files/luxun_color.jpg'))
+txt_book = path.join(d, d + '/files/luxun_book.txt')
+# Read the whole text. ‘rb’文件以二进制的方式读入
+text = open(txt_book, 'rb').read()
+
+wc = WordCloud(font_path=font_path, background_color="white", max_words=2000, mask=back_coloring,
+               max_font_size=100, random_state=42, width=1000, height=860, margin=2,)
+
+
+# wc.generate(jieba_processing_txt(text))
+
+wc.generate_from_frequencies(jieba_processing_txt(text))
+
+# create coloring from image
+image_colors_default = ImageColorGenerator(back_coloring)
+
+# save wordcloud
+wc.to_file(path.join(d, imgname2))
+
+# recolor wordcloud and show
+plt.figure()
+plt.imshow(wc, interpolation="bilinear")
+plt.axis("off")
+
+
+plt.figure()
+plt.imshow(back_coloring, interpolation="bilinear")
+plt.axis("off")
+
+# show
+# we could also give color_func=image_colors directly in the constructor
+# create coloring from image
+image_colors_byImg = ImageColorGenerator(back_coloring)
+plt.figure()
+plt.imshow(wc.recolor(color_func=image_colors_byImg), interpolation="bilinear")
+plt.axis("off")
+
+plt.show()
+
